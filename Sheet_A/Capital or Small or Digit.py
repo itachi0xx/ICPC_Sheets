864a0f3da586bd54f152d525a1acd47b5fd54283
itachi0xx <atiyahnasr@gmail.com>
2026-06-19 16:32:00 +0300

    problem # 13

---
+++ Sheet_A/Capital or Small or Digit.py
@@ -0,0 +1,12 @@
+def main():
+    x = input()
+
+    if x.isdigit():
+        print("IS DIGIT")
+    elif x.isupper():
+        print("Alpha \n IS CAPITAL")
+    else:
+        print("Alpha \n IS SMALL")
+
+
+main()
